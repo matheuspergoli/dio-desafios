e661cd10d49efb79d66ff5e863cbfefa42432b7f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-18 10:44:11 -0300

    Modificando fetch e adicionando botão e funcão

--- pokenext/components/Pokemons.tsx
+++ pokenext/components/Pokemons.tsx
@@ -2,27 +2,52 @@ import React from 'react'
 import { useQuery } from 'react-query'
 import CardPokemon from './CardPokemon'
 import { Data } from '../interface'
+import Button from './Button'
 
 function Pokemons() {
-	const { data } = useQuery(['pokemons-data'], fetchPokemons)
+	const [page, setPage] = React.useState(0)
+	const { data } = useQuery(['pokemons-data', page], fetchPokemons)
 
 	async function fetchPokemons() {
-		const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+		const response = await fetch(
+			`https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=20`
+		)
 		const json = await response.json()
 		return json as Data
 	}
 
+	function nextPage() {
+		setPage((currentPage) => currentPage + 20)
+	}
+
+	function prevPage() {
+		setPage((currentPage) => currentPage - 20)
+	}
+
 	return (
-		<div className='flex items-center justify-center gap-5 flex-wrap mx-auto max-w-5xl'>
-			{data &&
-				data.results.map((result, index) => (
-					<CardPokemon
-						key={index}
-						index={index}
-						url={result.url}
-					/>
-				))}
-		</div>
+		<>
+			<div className='flex items-center justify-center gap-5 flex-wrap mx-auto max-w-5xl'>
+				{data &&
+					data.results.map((result, index) => (
+						<CardPokemon
+							key={index}
+							index={index}
+							url={result.url}
+						/>
+					))}
+			</div>
+			<div className='flex justify-center gap-3 mt-5 mx-auto max-w-5xl'>
+				<Button
+					text='Prev'
+					onClick={prevPage}
+					disabled={page === 0}
+				/>
+				<Button
+					text='Next'
+					onClick={nextPage}
+				/>
+			</div>
+		</>
 	)
 }
 
