30f78319563df5abe33c60b8c9526eb654745d98
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-18 07:33:51 -0300

    Criando interface para dados da API

---
+++ pokenext/interface/index.ts
@@ -0,0 +1,21 @@
+export interface Data {
+	count: number
+	next: string
+	previous: null | string
+	results: { name: string; url: string }[]
+}
+
+export interface DataPokemon {
+	id: number
+	name: string
+	height: number
+	weight: number
+	sprites: {
+		front_default: string
+		other: {
+			'official-artwork': {
+				front_default: string
+			}
+		}
+	}
+}
