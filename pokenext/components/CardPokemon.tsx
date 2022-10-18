6ffd03d4d5925733246d21836f25db0389d5982c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-18 16:11:12 -0300

    Criando Modal de Pokemon

--- pokenext/components/CardPokemon.tsx
+++ pokenext/components/CardPokemon.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Modal } from '@mui/material'
 import { DataPokemon, TypePokemon } from '../interface'
 
+const styleModal = {
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'center',
+	height: '100vh',
+	width: '100vw'
+}
+
 function CardPokemon(props: { url: string; index: number }) {
+	const [open, setOpen] = React.useState(false)
 	const [data, setData] = React.useState<DataPokemon>(null)
 
 	React.useEffect(() => {
@@ -13,12 +24,21 @@ function CardPokemon(props: { url: string; index: number }) {
 		fetchPokemon()
 	})
 
+	function closeModal() {
+		setOpen(false)
+	}
+	function showModal() {
+		setOpen(true)
+	}
+
 	return (
 		<>
 			{data && (
 				<section className='flex flex-col items-center justify-center rounded-md w-64 h-80 bg-white'>
 					<span className='pl-5 self-start font-bold'>#{data.id}</span>
-					<figure>
+					<figure
+						className='cursor-pointer'
+						onClick={showModal}>
 						<img
 							className='w-40 h-40'
 							src={data.sprites.other['official-artwork'].front_default}
@@ -37,6 +57,34 @@ function CardPokemon(props: { url: string; index: number }) {
 							</li>
 						))}
 					</ul>
+					<Modal
+						sx={styleModal}
+						open={open}
+						onClose={closeModal}>
+						<section className='p-5 outline-none max-w-md w-full'>
+							<div className='p-2 rounded-md bg-white'>
+								<figure>
+									<img
+										className='mx-auto w-60'
+										src={data.sprites.other['official-artwork'].front_default}
+										alt={data.name}
+									/>
+								</figure>
+								<h1 className='text-center mb-5'>
+									<span className='capitalize text-2xl'>{data.name}</span>
+								</h1>
+								<ul className='flex justify-center gap-2'>
+									{data.types.map((type: TypePokemon) => (
+										<li
+											className='capitalize text-center rounded-lg border py-1 px-5 bg-red-600 text-white'
+											key={type.slot}>
+											{type.type.name}
+										</li>
+									))}
+								</ul>
+							</div>
+						</section>
+					</Modal>
 				</section>
 			)}
 		</>
