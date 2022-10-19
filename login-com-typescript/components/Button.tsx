4a03ac7da3f5e80585e744970108cada30cefbc3
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-19 09:29:21 -0300

    Criando botão do formulário

---
+++ login-com-typescript/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Button() {
+	return (
+		<button
+			type='submit'
+			className='px-3 py-2 rounded-full text-white bg-purple-700'>
+			Entrar
+		</button>
+	)
+}
+
+export default Button
