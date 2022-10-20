b0e0ddff27c0618b762f0a7cb1a41664e5241240
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-20 01:06:36 -0300

    Criando redirecionamento pelo servidor

--- dio-cadastro/pages/cadastro.tsx
+++ dio-cadastro/pages/cadastro.tsx
@@ -1,6 +1,27 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { getSession } from 'next-auth/react'
 import React from 'react'
 import Formulario from '../components/Formulario'
 import Menu from '../components/Menu'
+
+export const getServerSideProps: GetServerSideProps = async (
+	context: GetServerSidePropsContext
+) => {
+  const session = await getSession(context)
+  
+  if (session) {
+    return {
+      redirect: {
+        destination: '/logado',
+        permanent: false
+      }
+    }
+  }
+
+	return {
+		props: { user: '' }
+	}
+}
 
 function Cadastro() {
 	return (
